89ce32adfa3b4341a9ec3ce6966c65c83d380198
Lizeth Pilar <lquispegutierrez@uni.pe>
2017-06-16 13:50:09 -0500

    Update clase16.R

--- clase16.R
+++ clase16.R
@@ -1,3 +1,17 @@
 
-
+#Aestetic por ggplot2
 vignette("ggplot2-specs")
+
+#FACETS
+ggplot(mpg, aes(displ,hwy)) + geom_point() + facet_wrap(~class)
+#Distribution of en graphic displ vs. hwy in as many parts as class' values
+#If you use a continous variable instead os an atributte the result is ambiguos.
+
+geom_smooth()
+
+#An important argument of geom_smooth is method, because 
+#smoothing method (function) to use, eg. "lm", "glm", "gam", "loess", "rlm".
+#Calibrates smootheness 
+#loess, local polynomial regression fitting
+
+#jitter instead of point
